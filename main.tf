ff0df017be1ea7e03580d3adbeb912c8bffd3854
Akosua FRIMPONG <akosua.cloudsecurity@gmail.com>
2026-08-08 23:54:59 -0400

    Add public and private subnet configuration

--- main.tf
+++ main.tf
@@ -8,3 +8,35 @@ resource "aws_vpc" "main" {
     Name = "${var.environment}-secure-vpc"
   }
 }
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet-${count.index + 1}"
+  }
+} 
+# Available AWS Availability Zones
+
+data "aws_availability_zones" "available" {
+  state = "available"
+
+} 
+# Private Subnets
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.environment}-private-subnet-${count.index + 1}"
+  }
+}
