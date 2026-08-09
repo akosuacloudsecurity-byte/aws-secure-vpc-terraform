8e056228875d9a39db2535f7e64f4f0646ff13cb
Akosua FRIMPONG <akosua.cloudsecurity@gmail.com>
2026-08-09 00:27:35 -0400

    Add NAT gateway and private subnet routing

--- main.tf
+++ main.tf
@@ -69,3 +69,40 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.environment}-nat-eip"
+  }
+} # NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.environment}-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+} # Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+} # Private Route Table Associations
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
