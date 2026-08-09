e3c63e6826af82e46a73b33d9adb74c7080212b4
Akosua FRIMPONG <akosua.cloudsecurity@gmail.com>
2026-08-09 00:13:08 -0400

    Add public route table and internet route

--- main.tf
+++ main.tf
@@ -49,4 +49,17 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "${var.environment}-igw"
   }
+} 
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
 }
